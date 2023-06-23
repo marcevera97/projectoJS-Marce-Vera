024ea04b2609c604a623a8450f3c10114986ed32
marcelo vera <marcevera2834@gmail.com.ar>
2023-06-22 22:19:36 -0300

    agregue

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let hamburguesas = prompt ("Que desea llevar?");
+
+
+if (hamburguesas == "" ){
+
+
+    alert ("No se encontraron resultados")
+}
+
+
+else{ 
+
+    alert ("usted desea comprar"+ " " + hamburguesas);
+
+}
