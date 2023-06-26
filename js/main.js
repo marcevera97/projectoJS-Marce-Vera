44c4c90a0bd72052b4c163e4ae1667e829c8d68f
marcelo vera <marcevera2834@gmail.com.ar>
2023-06-26 03:27:37 -0300

    cambios en HTML y JS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,59 @@
-let hamburguesas = prompt ("Que desea llevar?");
 
+let promoFiesta = parseInt(prompt("ingrese sus promos a comprar"));
 
-if (hamburguesas == "" ){
+if (isNaN(promoFiesta)) {
 
+    alert("por favor ingrese un numero")
 
-    alert ("No se encontraron resultados")
 }
 
+else if (promoFiesta < 0) {
 
-else{ 
+    alert("por favor, ingrese un numero mayou a 1")
 
-    alert ("usted desea comprar"+ " " + hamburguesas);
+}
+
+else {
+
+    let totalDeCompra = 0;
+    let listaDeProductosSeleccionados = "";
+
+
+    for (let i = 0; i < promoFiesta; i++) {
+
+        let producto = parseInt(prompt("ingrese el numero de promo"));
+
+        if (producto == 1) {
+
+            listaDeProductosSeleccionados += "promo cumple 1 - $5500" + "<br>";
+            totalDeCompra += 5500;
+
+        }
+        else if (producto == 2) {
+
+            listaDeProductosSeleccionados += "promo cumple 2 - $6800" + "<br>";
+            totalDeCompra += 6800;
+        }
+        else if (producto == 3) {
+
+            listaDeProductosSeleccionados += "promo cumple 3 - $6800" + "<br>";
+            totalDeCompra += 6800;
+        }
+        else if (producto == 4) {
+
+            listaDeProductosSeleccionados += "promo cumple 4 - $6700" + "<br>";
+            totalDeCompra += 6700;
+        }
+
+        else {
+
+            alert("producto no encontrado")
+        }
+
+
+    }
+
+    document.write("lista de articulos seleccionados :<br> " + listaDeProductosSeleccionados + "<br>");
+    document.write("el total de la compra es : $ " + totalDeCompra);
 
 }
